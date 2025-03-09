f1f877744937869ae92dd81d18f33dedf5bb07a9
$$jlhk <julienelachkar@gmail.com>
2025-03-09 23:46:15 +0100

    fix: remove invalid next.config.js options

--- next.config.js
+++ next.config.js
@@ -8,20 +8,6 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { net: false, dns: false };
     return config;
-  },
-  httpAgentOptions: {
-    family: 4
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  },
-  env: {
-    UV_THREADPOOL_SIZE: '64',
-    NODE_OPTIONS: '--dns-result-order=ipv4first',
-    HOSTNAME: '0.0.0.0',
-    HOST: '0.0.0.0',
-    PORT: '3000'
   }
 };
 
